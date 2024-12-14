6d339315451b1feac71e238139fcd9860695d6cf
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2024-12-14 15:14:21 -0800

    more done

--- bot/src/humanoid.rs
+++ bot/src/humanoid.rs
@@ -3,4 +3,9 @@ pub trait Humanoid {
     async fn calibrate(&mut self) -> Result<(), ()>;
 
     async fn set_left_shoulder_yaw(&mut self, yaw: f32) -> Result<(), ()>;
+
+    async fn set_left_elbow_yaw(&mut self, yaw: f32) -> Result<(), ()>;
+
+    async fn set_right_eblow_yaw(&mut self, yaw: f32) -> Result<(), ()>;
+
 }
--- bot/src/mini_robot.rs
+++ bot/src/mini_robot.rs
@@ -1,5 +1,6 @@
 use bon::Builder;
-use zeroth::JointPosition;
+use tokio::io::Join;
+use zeroth::{JointPosition, ServoId};
 
 use crate::humanoid::Humanoid;
 
@@ -12,6 +13,10 @@ pub struct MiniRobot {
 pub struct MiniRobotCalibration {
     pub left_shoulder_yaw_min: f32,
     pub left_shoulder_yaw_max: f32,
+    pub left_elbow_yaw_min: f32,
+    pub left_elbow_yaw_max: f32,
+    pub right_elbow_yaw_min: f32,
+    pub right_elbow_yaw_max: f32,
 }
 
 impl MiniRobot {
@@ -23,40 +28,82 @@ impl MiniRobot {
     }
 }
 
- impl Humanoid for MiniRobot {
-       async  fn calibrate(&mut self) -> Result<(), ()> {
+impl Humanoid for MiniRobot {
+    async fn calibrate(&mut self) -> Result<(), ()> {
         let left_shoulder_yaw_info = self.client.get_servo_info(15).await.unwrap().unwrap();
+        let left_elbow_yaw_info = self
+            .client
+            .get_servo_info(ServoId::LeftElbowYaw as u32)
+            .await
+            .unwrap()
+            .unwrap();
+        let right_elbow_yaw_info = self
+            .client
+            .get_servo_info(ServoId::RightElbowYaw as u32)
+            .await
+            .unwrap()
+            .unwrap();
 
         let calibration_builder = MiniRobotCalibration::builder()
             .left_shoulder_yaw_max(left_shoulder_yaw_info.max_position)
-            .left_shoulder_yaw_min(left_shoulder_yaw_info.min_position);
+            .left_shoulder_yaw_min(left_shoulder_yaw_info.min_position)
+            .left_elbow_yaw_max(left_elbow_yaw_info.max_position)
+            .left_elbow_yaw_min(left_elbow_yaw_info.min_position)
+            .right_elbow_yaw_max(right_elbow_yaw_info.max_position)
+            .right_elbow_yaw_min(right_elbow_yaw_info.min_position);
 
         self.calibration = Some(calibration_builder.build());
 
         Ok(())
-        // self.client.start_calibration(
-        //     Request::new(
-        //         CalibrationRequest {
-
-        //         }
-        //     )
-        // )
     }
 
     async fn set_left_shoulder_yaw(&mut self, yaw: f32) -> Result<(), ()> {
-        
         let calibration = self.calibration.clone().unwrap();
-        let yaw = yaw * (calibration.left_shoulder_yaw_max - calibration.left_shoulder_yaw_min) / 90.0 + calibration.left_shoulder_yaw_min;
-
+        let yaw = yaw * (calibration.left_shoulder_yaw_max - calibration.left_shoulder_yaw_min)
+            / 90.0
+            + calibration.left_shoulder_yaw_min;
 
         let _ = self
             .client
             .set_position(JointPosition {
-                id: 15,
+                id: ServoId::LeftShoulderYaw as i32,
                 position: yaw,
                 speed: 100.0,
             })
             .await;
+        Ok(())
+    }
+
+    async fn set_left_elbow_yaw(&mut self, yaw: f32) -> Result<(), ()> {
+        let calibration = self.calibration.clone().unwrap();
+        let yaw = (yaw + 90.0) * (calibration.left_elbow_yaw_max - calibration.left_elbow_yaw_min)
+            / 180.0
+            + calibration.left_elbow_yaw_min;
+
+        self.client
+            .set_position(JointPosition {
+                id: ServoId::LeftElbowYaw as i32,
+                position: yaw,
+                speed: 100.0,
+            })
+            .await
+            .unwrap();
+        Ok(())
+    }
+
+    async fn set_right_eblow_yaw(&mut self, yaw: f32) -> Result<(), ()> {
+        let calibration = self.calibration.clone().unwrap();
+        let yaw = (90.0 - yaw) * (calibration.right_elbow_yaw_max - calibration.right_elbow_yaw_min)
+            / 180.0
+            + calibration.right_elbow_yaw_min;
+
+        self.client.set_position(
+            JointPosition {
+                id: ServoId::RightElbowYaw as i32,
+                position: yaw,
+                speed: 100.0
+            }
+        ).await.unwrap();
         Ok(())
     }
 }
