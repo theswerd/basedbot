287a3c4c8b451286bb6656d73df1611bf8bb4af6
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2024-12-14 14:32:43 -0800

    movement

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -1,43 +1,30 @@
 use std::net::TcpStream;
 
 use tonic::{transport::Channel, Request};
-use zeroth::{CalibrationRequest, JointPosition, ServoId, TorqueSetting, WifiCredentials};
+// use zeroth::{CalibrationRequest, JointPosition, ServoId, TorqueSetting, WifiCredentials};
 use bon::{builder, Builder};
-
-pub fn empty_request() -> tonic::Request<zeroth::Empty> {
-    return tonic::Request::new(zeroth::Empty {});
-}
-
+use zeroth::JointPosition;
 
 
 #[tokio::main]
 async fn main() {
-    let client =
-        zeroth::servo_control_client::ServoControlClient::connect("grpc://192.168.42.1:50051")
-            .await;
+    let client = zeroth::Client::connect("grpc://192.168.42.1:50051").await;
 
     let mut client = match client {
         Ok(client) => client,
         Err(e) => panic!("Failed to connect to the server: {:?}", e),
     };
 
-    let _ = client.disable_movement(
-        empty_request()
-    ).await;
+    client.disable_movement().await.unwrap();
 
-    let mut robot = MiniRobot::new(
-        client
-    );
+    let mut robot = MiniRobot::new(client);
 
-   let _ = robot.set_left_shoulder_yaw(129.0).await;
-
-
+    robot.set_left_shoulder_yaw(129.0).await.unwrap();
 }
 
-
 pub struct MiniRobot {
-    client:zeroth::servo_control_client::ServoControlClient<Channel>,
-    calibration:Option<MiniRobotCalibration>,
+    client: zeroth::Client,
+    calibration: Option<MiniRobotCalibration>,
 }
 
 #[derive(Builder)]
@@ -47,52 +34,37 @@ pub struct MiniRobotCalibration {
 }
 
 impl MiniRobot {
-    pub fn new(client: zeroth::servo_control_client::ServoControlClient<Channel>) -> Self{
+    pub fn new(client: zeroth::Client) -> Self {
         return MiniRobot {
             client,
-            calibration: None
-        }
+            calibration: None,
+        };
     }
 }
 
 pub trait Humanoid {
+    async fn calibrate(&mut self) -> Result<(), ()>;
 
-    async fn calibrate(
-        &mut self, 
-    ) -> Result<(), ()>;
-
-    async fn set_left_shoulder_yaw(
-        &mut self,
-        yaw:f32
-    ) -> Result<(), ()>;
+    async fn set_left_shoulder_yaw(&mut self, yaw: f32) -> Result<(), ()>;
 }
 
 impl Humanoid for MiniRobot {
+    async fn calibrate(&mut self) -> Result<(), ()> {
+        // let left_shoulder_yaw_info = self
+        //     .client
+        //     .get_(Request::new(ServoId { id: 15 }))
+        //     .await
+        //     .unwrap();
 
-    async fn calibrate(
-            &mut self, 
-        ) -> Result<(), ()> {
-            let left_shoulder_yaw_info  = self.client.get_servo_info(
-                Request::new(
-                    ServoId {
-                        id: 15
-                    }
-                )
-            ).await.unwrap();
+        // let yaw = if let zeroth::servo_info_response::Result::Info(yaw) =
+        //     left_shoulder_yaw_info.into_inner().result.unwrap()
+        // {
+        //     yaw
+        // } else {
+        //     return Err(());
+        // };
 
-            let yaw = if let zeroth::servo_info_response::Result::Info(yaw ) =  left_shoulder_yaw_info.into_inner().result.unwrap() {
-                yaw
-            } else {
-                return Err(())
-            };
-
-            
-
-
-            
-
-
-            Ok(())
+        Ok(())
         // self.client.start_calibration(
         //     Request::new(
         //         CalibrationRequest {
@@ -102,22 +74,17 @@ impl Humanoid for MiniRobot {
         // )
     }
 
-    async fn set_left_shoulder_yaw(
-         &mut self,
-        yaw: f32
-    ) -> Result<(), ()> {
-        
-        let _ = self.client.set_position(
-            Request::new(
+    async fn set_left_shoulder_yaw(&mut self, yaw: f32) -> Result<(), ()> {
+        let _ = self
+            .client
+            .set_position(
                 JointPosition {
                     id: 15,
                     position: yaw,
-                    speed: 100.0
+                    speed: 100.0,
                 }
             )
-            // 15
-
-        ).await;
+            .await;
         Ok(())
     }
 }
