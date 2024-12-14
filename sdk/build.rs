a8bdef27c4a905a0e8a31e61c3ea9ae52b985ff2
Will Hopkins <willothyh@gmail.com>
2024-12-14 12:36:15 -0800

    generate protobuf sdk

---
+++ sdk/build.rs
@@ -0,0 +1,8 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tonic_build::configure()
+        .build_client(true)
+        .build_server(false)
+        .compile_protos(&["proto/hal_pb.proto"], &["proto"])?;
+
+    Ok(())
+}
