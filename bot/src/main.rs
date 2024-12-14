eb3d1ffff8f3d345fec6a357041a06e453e10a91
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2024-12-14 13:11:40 -0800

    god damn it

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -1,4 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpStream;
+
+#[tokio::main]
+async fn main() {
     
+    let client = zeroth::servo_control_client::ServoControlClient::connect(
+        "grpc://192.168.42.1"
+    ).await;
+
+
+
+    
+    println!("Hello, world: {:?}", client);
+
 }
