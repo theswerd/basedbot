b3c9976ded0815e3f899f377562772421e959468
Will Hopkins <willothyh@gmail.com>
2024-12-14 16:36:01 -0800

    granular joint setting

--- bot/src/humanoid.rs
+++ bot/src/humanoid.rs
@@ -1,64 +1,87 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Copy, Serialize, Deserialize, PartialEq, Eq)]
+pub enum Joint {
+    LeftHipPitch,
+    LeftHipYaw,
+
+    RightHipPitch,
+    RightHipYaw,
+
+    LeftKneePitch,
+    LeftKneeYaw,
+
+    RightKneePitch,
+    RightKneeYaw,
+
+    LeftAnklePitch,
+    LeftAnkleYaw,
+
+    RightAnklePitch,
+    RightAnkleYaw,
+
+    LeftShoulderPitch,
+    LeftShoulderYaw,
+
+    RightShoulderPitch,
+    RightShoulderYaw,
+
+    LeftElbowPitch,
+    LeftElbowYaw,
+
+    RightElbowPitch,
+    RightElbowYaw,
+
+    LeftWristPitch,
+    LeftWristYaw,
+
+    RightWristPitch,
+    RightWristYaw,
+
+    NeckPitch,
+    NeckYaw,
+}
+
+impl Into<Option<zeroth::ServoId>> for Joint {
+    fn into(self) -> Option<zeroth::ServoId> {
+        use zeroth::ServoId::*;
+        Some(match self {
+            Joint::LeftHipPitch => LeftHipPitch,
+            Joint::LeftHipYaw => LeftHipYaw,
+            Joint::RightHipPitch => RightHipPitch,
+            Joint::RightHipYaw => RightHipYaw,
+            Joint::LeftKneePitch => LeftKneePitch,
+            Joint::LeftKneeYaw => None?,
+            Joint::RightKneePitch => RightAnklePitch,
+            Joint::RightKneeYaw => None?,
+            Joint::LeftAnklePitch => LeftAnklePitch,
+            Joint::LeftAnkleYaw => None?,
+            Joint::RightAnklePitch => RightAnklePitch,
+            Joint::RightAnkleYaw => None?,
+            Joint::LeftShoulderPitch => LeftShoulderPitch,
+            Joint::LeftShoulderYaw => LeftShoulderYaw,
+            Joint::RightShoulderPitch => RightShoulderPitch,
+            Joint::RightShoulderYaw => RightShoulderYaw,
+            Joint::LeftElbowPitch => None?,
+            Joint::LeftElbowYaw => LeftElbowYaw,
+            Joint::RightElbowPitch => None?,
+            Joint::RightElbowYaw => RightElbowYaw,
+            Joint::LeftWristPitch
+            | Joint::LeftWristYaw
+            | Joint::RightWristPitch
+            | Joint::RightWristYaw
+            | Joint::NeckPitch
+            | Joint::NeckYaw => None?,
+        })
+    }
+}
+
 pub trait Humanoid {
     fn calibrate(&mut self) -> impl std::future::Future<Output = eyre::Result<()>> + Send;
 
-    fn set_left_shoulder_yaw(
+    fn set_joint(
         &mut self,
-        yaw: f32,
+        joint: Joint,
+        value: f32,
     ) -> impl std::future::Future<Output = eyre::Result<()>> + Send;
-
-    fn set_left_elbow_yaw(
-        &mut self,
-        yaw: f32,
-    ) -> impl std::future::Future<Output = eyre::Result<()>> + Send;
-
-    fn set_right_shoulder_yaw(
-        &mut self,
-        yaw: f32,
-    ) -> impl std::future::Future<Output = eyre::Result<()>> + Send;
-
-    fn set_right_elbow_yaw(
-        &mut self,
-        yaw: f32,
-    ) -> impl std::future::Future<Output = eyre::Result<()>> + Send;
-
-    fn set_left_hip_yaw(
-        &mut self,
-        yaw: f32,
-    ) -> impl std::future::Future<Output = eyre::Result<()>> + Send;
-
-    fn set_left_hip_pitch(
-        &mut self,
-        yaw: f32,
-    ) -> impl std::future::Future<Output = eyre::Result<()>> + Send;
-
-    fn set_right_hip_yaw(
-        &mut self,
-        yaw: f32,
-    ) -> impl std::future::Future<Output = eyre::Result<()>> + Send;
-
-    fn set_right_hip_pitch(
-        &mut self,
-        yaw: f32,
-    ) -> impl std::future::Future<Output = eyre::Result<()>> + Send;
-
-    fn set_right_shoulder_pitch(
-        &mut self,
-        pitch: f32,
-    ) -> impl std::future::Future<Output = eyre::Result<()>> + Send;
-
-    fn set_left_shoulder_pitch(
-        &mut self,
-        pitch: f32,
-    ) -> impl std::future::Future<Output = eyre::Result<()>> + Send;
-
-    fn set_left_ankle_pitch(
-        &mut self,
-        pitch: f32,
-    ) -> impl std::future::Future<Output = eyre::Result<()>> + Send;
-
-    fn set_right_ankle_pitch(
-        &mut self,
-        pitch: f32,
-    ) -> impl std::future::Future<Output = eyre::Result<()>> + Send;
-
 }
