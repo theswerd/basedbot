c686ff39f5be207f4f38af3073106a9114c83c2e
Matze99 <hamachermatthias12@gmail.com>
2024-12-15 11:56:20 -0800

    added output

--- big_robot/build.rs
+++ big_robot/build.rs
@@ -1,20 +1,43 @@
+use std::env;
+use std::path::PathBuf;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Path to the Protobuf files
+    let proto_root = "proto";
+    
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    // List of Protobuf files
+    let protos = [
+        "kos/common.proto",
+        "kos/actuator.proto",
+        "kos/imu.proto",
+        "kos/inference.proto",
+        "kos/process_manager.proto",
+        "kos/system.proto",
+        "google/longrunning/operations.proto",
+    ];
+
+    std::fs::create_dir_all(out_dir.join("kos")).expect("Failed to create output directory");
+
     tonic_build::configure()
         .build_client(true)
         .build_server(false)
         .protoc_arg("--experimental_allow_proto3_optional")
         .build_transport(true)
         .compile_well_known_types(true)
+        .out_dir(out_dir.join("kos"))
         .emit_rerun_if_changed(true)
-        .compile_protos(&[
-            "kos/actuator.proto",
-            "kos/common.proto",
-            "kos/imu.proto",
-            "kos/inference.proto",
-            "kos/process_manager.proto",
-            "kos/system.proto",
-            "google/longrunning/operations.proto",
-            ], &["proto", &format!("{}/googleapis", "proto")])?;
+        .compile_protos(
+            &protos,
+            &["proto", &format!("{}/googleapis", "proto")],
+        )?;
+
+    // Re-run the build script if any of the proto files change
+    for proto in &protos {
+        println!("cargo:rerun-if-changed={}/kos/{}", proto_root, proto);
+    }
+    println!("cargo:rerun-if-changed={}", proto_root);
 
     Ok(())
 }
